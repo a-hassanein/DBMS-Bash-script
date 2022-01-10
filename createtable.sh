8f72e6adefa62ff09e2ae147b6bf0606545edc39
Abdelrahman <abdelrahmanmohamedkhaleel@gmail.com>
2022-01-11 00:34:25 +0200

    createtable

--- createtable.sh
+++ createtable.sh
@@ -1,10 +1,105 @@
 #!/bin/bash
+function datatypeMenu(){
+	datatypeMenu=$(whiptail --title "Data Type Menu " --fb --menu "select Data Type" 15 60 4 \
+		"1" "int"
+		"2" "str"
+		"3" "boolean" 3>&1 1>&2 2>&3)
+		
+		case datatypeMenu in 
+			1) 
+				datatype="int"
+				;;
+
+			2) 
+				datatype="str"
+				;;
+			3)
+				datatype="boolean"
+				;;
+		esac
+	}
+
+
 
 if [[ -f $tableName ]] ; then
 	whiptail --title "Error" --msgbox "This table is already exist" 8 45
 else
 	colNumber=$(whiptail --title "column Number" --inputbox "Enter column number" 8 45 3>&1 1>&2 2>&3)
 	touch $tableName
+	touch .$tableName
 	echo $colNumber >> $tableName
 	echo "you are sucessfully created a table"
+
+	i=1
+	datatype=""
+	fasel=":"
+	isPrimary=""
+	primarykeyMenu="2"
+	tableInfo="$colName$fasel$datatype$fasel$isPrimary"
+
+	while [ $i -le $colNumber ]
+	do
+		
+        	colName=$(whiptail --title "column Name" --inputbox "Enter Column $i Name "  8 45 3>&1 1>&2 2>&3)
+
+		     datatypeMenu=$(whiptail --title "Data Type Menu " --fb --menu "select Data Type" 15 60 4 \
+                  	 "1" "int" \
+                  	 "2" "str" \
+                   	 "3" "boolean" 3>&1 1>&2 2>&3)
+   
+                   case $datatypeMenu in
+                           1)
+                                  datatype="int"
+                                  ;;
+  
+                          2)
+                                 datatype="str"
+                                  ;;
+                          3)
+                                 datatype="boolean"
+                                  ;;
+                  esac
+
+		  if [[ $primarykeyMenu == "2" ]]; then
+
+
+		  primarykeyMenu=$(whiptail --title "Primary Key Menu " --fb --menu "Is column primary key?" 15 60 4 \
+			"1" "yes" \
+			"2" "no" 3>&1 1>&2 2>&3)
+		  case $primarykeyMenu in
+			1)
+				 isPrimary="PK"
+				 ;;
+			2)
+				 isPrimary=""
+				 ;;
+		  esac
+
+
+
+		  fi
+		
+		
+		  if [[ i -eq $colNumber ]]; then
+			    echo -n $colName >> $tableName;
+		    else
+			      echo -n $colName$fasel >> $tableName;
+
+		  fi
+        	 
+		echo -n $colName$fasel$datatype$fasel$isPrimary$fasel >> .$tableName;
+
+
+
+        	((i++))
+		
+		isPrimary=""	
+
+	done
+	
 fi
+
+cd .. 
+. ./connectdb.sh
+cd $dbName
+
